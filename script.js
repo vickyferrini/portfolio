2dc8aac48c22ade0398a6b1ed8e2e72ed301512e
vickyferrini <vickyferrini@gmail.com>
2021-11-04 19:13:31 -0300

    cambios

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (tthis.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        }else{
+           
+        }
+    })
+});
